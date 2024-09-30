611313c3930c205010b893626f87c63d9d62d8c4
Luis Fernando Arevalo <luisferarevalou@gmail.com>
2024-09-30 04:03:20 -0500

    uno otrav ez

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,4 +4,11 @@ controller.inicio = (req, res) => {
   res.render('main')
 }
 
+controller.single = (req, res) => {
+  const mes = req.body.periodo
+  console.log(mes)
+
+  res.send('enviado')
+}
+
 export { controller }
--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,10 @@ import { controller } from "../controllers/mainController.js";
 const app = express()
 
 router.get('/', controller.inicio)
+router.post('/upload', controller.single)
+
 
 
 export { router } 
+
+// upload.single('file') 
