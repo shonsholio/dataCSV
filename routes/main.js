a720faa3ad172f498a36456b2e12c9d49e66a7a0
Luis Fernando Arevalo <luisferarevalou@gmail.com>
2024-09-30 04:07:07 -0500

    con multer

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,14 @@
 import express from "express";
 const router = express.Router()
 import { controller } from "../controllers/mainController.js";
+import multer from "multer";
 
+const upload = multer({ dest: './uploads/' });
 const app = express()
 
 router.get('/', controller.inicio)
-router.post('/upload', controller.single)
+router.post('/upload', upload.single('file'), controller.single)
 
 
 
 export { router } 
-
-// upload.single('file') 
